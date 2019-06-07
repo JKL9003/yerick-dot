cae61937355065b1536475a5f21c11fc925a1715
Justin Liles <Justin.Liles@Live.com>
2019-06-06 23:56:51 -0500

    Updated for me.

--- install.sh
+++ install.sh
@@ -5,15 +5,13 @@ sudo apt-get upgrade
 
 sudo apt-get install git liblua5.3* libsdl2* synaptic steam vim snapd-xdg-open
 
-git config --global user.email "yerick147083@gmail.com"
-git config --global user.name "EricWRogers"
+git config --global user.email "Justin.Liles@Live.com"
+git config --global user.name "Justin Liles"
 
 git config --global credential.helper store
+
 # Set git default editor to vim
 sudo git config --system core.editor vim
-
-#cd ../
-#git clone https://github.com/EricWRogers/SoftwareEngineeringClass.git 
 
 # VS_Code
 sudo snap install code --classic --stable
@@ -26,15 +24,8 @@ sudo apt-get install apt-transport-https
 sudo apt-get update
 sudo apt-get install dotnet-sdk-2.2
 
-# DotNet Core
-#sudo snap install dotnet-sdk --classic
-#sudo snap alias dotnet-sdk.dotnet dotnet
-
 # Slack
 sudo snap install slack --classic
 
 # Discord
 sudo snap install discord --classic
-
-# Chromium
-sudo snap install chromium
