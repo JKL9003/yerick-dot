4a052f7415d80604d2e217ca46c001f525b5eb67
Justin Liles <Justin.Liles@outlook.com>
2023-07-07 10:24:33 -0500

    Updated VBox setup

--- Virual Box Setup.sh
+++ Virual Box Setup.sh
@@ -5,4 +5,4 @@ sudo apt-get upgrade
 
 sudo apt-get install linux-headers-$(uname -r) build-essential dkms build-essential﻿
 
-#sudo /VBoxLinuxAdditions.run
+sudo sudo /media/jkl9003/VBox_GAs7.0.8/VBoxLinuxAdditions.run
